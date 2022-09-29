6b1a13ed56b81571ff17e68993e37be1f881777b
ahfercen <ahfercen@gmail.com>
2022-09-29 02:09:13 -0400

    Play Against Computer added

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ function getCompChoice(){
     return HAND[rand]
 }
 
+function playAgainstComp(playerHand){
+    let compHand = getCompChoice();
+    switch(playerHand){
+        case HAND[0]:
+            if(compHand == HAND[0]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nTie!`);
+            }else if(compHand == HAND[1]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nComp wins!`);
+            }else if(compHand == HAND[2]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nPlayer wins!`);
+            }
+            break;
+        case HAND[1]:
+            if(compHand == HAND[0]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nPlayer win!`);
+            }else if(compHand == HAND[1]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nTie!`);
+            }else if(compHand == HAND[2]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nComp wins!`);
+            }
+            break;
+        case HAND[2]:
+            if(compHand == HAND[0]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nComp Wins!`);
+            }else if(compHand == HAND[1]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nPlayer Wins!`);
+            }else if(compHand == HAND[2]){
+                console.log(`Player: ${playerHand},\nComp: ${compHand},\nTie!`);
+            }
+            break;
+        default:
+            console.log("ERROR: undefined hand, please choose a valid hand.");
+            break;
+    }
+};
