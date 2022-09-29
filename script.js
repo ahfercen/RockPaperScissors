616c1ab018ab34299af06a29f74cde478bdc6347
ahfercen <ahfercen@gmail.com>
2022-09-29 02:21:47 -0400

    Score keeping

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const HAND = ['rock','paper','scissors']
-
-function getCompChoice(){
-    let rand = Math.floor(Math.random() * 3)
-    return HAND[rand]
+const HAND = ['rock','paper','scissors'];
+let playerScore = 0;
+let compScore = 0;
+function playerWin(bool){
+    if(bool === true){
+        playerScore++;
+    }else{
+        compScore++;
+    }
 }
+function resetScore(){
+    playerScore = 0;
+    compScore = 0;
+}
+function game(){
+    for(let round = 0; round <5; round++){
+        console.log(playAgainstComp(prompt("Rock, Paper, or Scissors?")));
+        
+    }
+}
+function getCompChoice(){
+    let rand = Math.floor(Math.random() * 3);
+    return HAND[rand];
+};
 function sanitizePlayerInput(s){
     return s.toLowerCase();
-}
+};
 function playAgainstComp(pH){
     let playerHand = sanitizePlayerInput(pH);
     let compHand = getCompChoice();
@@ -15,24 +33,30 @@ function playAgainstComp(pH){
             if(compHand == HAND[0]){
                 return(`Player: ${playerHand},\nComp: ${compHand},\nTie!`);
             }else if(compHand == HAND[1]){
+                playerWin(false);
                 return(`Player: ${playerHand},\nComp: ${compHand},\nComp wins!`);
             }else if(compHand == HAND[2]){
+                playerWin(true);
                 return(`Player: ${playerHand},\nComp: ${compHand},\nPlayer wins!`);
             }
             break;
         case HAND[1]:
             if(compHand == HAND[0]){
+                playerWin(true);
                 return(`Player: ${playerHand},\nComp: ${compHand},\nPlayer win!`);
             }else if(compHand == HAND[1]){
                 return(`Player: ${playerHand},\nComp: ${compHand},\nTie!`);
             }else if(compHand == HAND[2]){
+                playerWin(false);
                 return(`Player: ${playerHand},\nComp: ${compHand},\nComp wins!`);
             }
             break;
         case HAND[2]:
             if(compHand == HAND[0]){
+                playerWin(false);
                 return(`Player: ${playerHand},\nComp: ${compHand},\nComp Wins!`);
             }else if(compHand == HAND[1]){
+                playerWin(true);
                 return(`Player: ${playerHand},\nComp: ${compHand},\nPlayer Wins!`);
             }else if(compHand == HAND[2]){
                 return(`Player: ${playerHand},\nComp: ${compHand},\nTie!`);
